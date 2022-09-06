6ffe47c01ba76a311cbe50267c518f0cbd48f248
sk-iv <iva6584@gmail.com>
2022-09-06 14:35:05 +0300

    fix init local storage

--- host/src/model2.js
+++ host/src/model2.js
@@ -6,7 +6,7 @@ export const toggle = createEvent('toggle')
 const togglerLocalStorage = connectLocalStorage("featureActive")
   .onError((err) => console.log(err))
 
-const $isChecked = createStore(togglerLocalStorage.init(), {name: '$isChecked'})
+const $isChecked = createStore(togglerLocalStorage.init(true), {name: '$isChecked'})
   .on(toggle, state => !state)
 
 $isChecked.watch(togglerLocalStorage)
